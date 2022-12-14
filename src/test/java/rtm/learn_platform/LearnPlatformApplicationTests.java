4aa922dc565daf39b7ddb1735eec53ebdac9bcf1
YergaliyevDauren <116344084+YergaliyevDauren@users.noreply.github.com>
2022-12-14 14:13:55 +0600

    Initial commit

---
+++ src/test/java/rtm/learn_platform/LearnPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package rtm.learn_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
